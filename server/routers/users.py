ea0811e79b6cd7302587c726f2af117c53415815
Marley Mulvin Broome <marley.developer@gmail.com>
2023-10-04 17:32:09 +0900

    pyrebase4を追加した

--- server/routers/users.py
+++ server/routers/users.py
@@ -6,7 +6,3 @@ from fastapi import APIRouter
 
 user_router = APIRouter()
 
-
-@user_router.get("/users/", tags=["users"])
-async def read_users():
-    return [{"username": "Foo"}, {"username": "Bar"}]
