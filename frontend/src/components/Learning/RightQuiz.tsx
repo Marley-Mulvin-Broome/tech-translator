e442f2cbf8e99195df45247e87a0a4131d74908c
daikiando <daikiando@andoutaikinoMacBook-Pro.local>
2023-10-07 23:34:38 +0900

    aaaa

--- frontend/src/components/Learning/RightQuiz.tsx
+++ frontend/src/components/Learning/RightQuiz.tsx
@@ -1,5 +1,22 @@
 import React, { useState, useEffect, ChangeEvent } from 'react';
-import { Container, Typography, Button, RadioGroup, FormControlLabel, Radio, LinearProgress } from '@mui/material';
+import {
+  Container,
+  Typography,
+  Button,
+  Checkbox,
+  LinearProgress,
+  Box,
+  Dialog,
+  DialogTitle,
+  DialogContent,
+  DialogContentText,
+  DialogActions,
+  Grid,
+  FormGroup,
+} from '@mui/material';
+import ArrowForwardIcon from '@mui/icons-material/ArrowForward';
+import CheckIcon from '@mui/icons-material/Check';
+import StarIcon from '@mui/icons-material/Star';
 
 interface Choice {
   text: string;
@@ -8,18 +25,49 @@ interface Choice {
 const RightQuiz: React.FC = () => {
   const [question, setQuestion] = useState<string>('');
   const [answerOptions, setAnswerOptions] = useState<string[]>([]);
-  const [correctAnswer, setCorrectAnswer] = useState<string>('');
-  const [selectedAnswer, setSelectedAnswer] = useState<string>('');
+  const [correctAnswers, setCorrectAnswers] = useState<string[]>([]);
+  const [selectedAnswers, setSelectedAnswers] = useState<string[]>([]);
   const [loading, setLoading] = useState<boolean>(false);
-  const [initialLoad, setInitialLoad] = useState<boolean>(true);
   const [showResult, setShowResult] = useState<boolean>(false);
   const [isCorrect, setIsCorrect] = useState<boolean>(false);
+  const [score, setScore] = useState<number>(0);
+  const [questionCount, setQuestionCount] = useState<number>(0);
+  const [quizStarted, setQuizStarted] = useState<boolean>(false);
+  const [shouldRestartQuiz, setShouldRestartQuiz] = useState<boolean>(false);
+  const [openDialog, setOpenDialog] = useState<boolean>(false);
+  const [isRainingStars, setIsRainingStars] = useState<boolean>(false);
+
+  const handleStartQuiz = () => {
+    setQuizStarted(true);
+    generateQuestion();
+  };
+
+  const handleRestartQuiz = () => {
+    setShouldRestartQuiz(false);
+    setQuestionCount(0);
+    setScore(0);
+    setOpenDialog(false);
+    generateQuestion();
+  };
+
+  const handleCloseDialog = () => {
+    setOpenDialog(false);
+  };
+
+  const handleNextQuestion = () => {
+    if (questionCount < 10) {
+      generateQuestion();
+    } else {
+      setOpenDialog(true);
+    }
+  };
 
   const generateQuestion = async () => {
     setLoading(true);
     try {
+      // 問題を生成するAPIコールを行う
       const apiKey = process.env.REACT_APP_OPENAI_API_KEY;
-      const prompt = 'Generate a quiz question:';
+      const prompt = 'Generate a quiz question in English:';
 
       const response = await fetch('https://api.openai.com/v1/engines/davinci/completions', {
         method: 'POST',
@@ -28,17 +76,22 @@ const RightQuiz: React.FC = () => {
           'Authorization': `Bearer ${apiKey}`,
         },
         body: JSON.stringify({
-          prompt, // プロンプトを指定
-          max_tokens: 30, // 最大トークン数を調整
-          temperature: 0.7, // ランダム性を調整
+          prompt,
+          max_tokens: 30,
+          temperature: 0.7,
         }),
       });
 
       const data = await response.json();
-      if (data.choices[0] && data.choices[0].text) {
-        setQuestion(data.choices[0].text);
 
-        // 正しい回答を取得
+      if (data.choices[0] && data.choices[0].text) {
+        const generatedQuestion = data.choices[0].text;
+
+        // 問題文をセット
+        setQuestion(generatedQuestion);
+
+        // 問題に対する正しい回答を取得するAPIコールを行う
+        const correctAnswerPrompt = `What is the answer to this question: ${generatedQuestion}`;
         const correctAnswerResponse = await fetch('https://api.openai.com/v1/engines/davinci/completions', {
           method: 'POST',
           headers: {
@@ -46,17 +99,20 @@ const RightQuiz: React.FC = () => {
             'Authorization': `Bearer ${apiKey}`,
           },
           body: JSON.stringify({
-            prompt: `What is the answer to this question: ${data.choices[0].text}`,
+            prompt: correctAnswerPrompt,
             max_tokens: 10,
             temperature: 0.7,
           }),
         });
+
         const correctAnswerData = await correctAnswerResponse.json();
         const correctAnswerText = correctAnswerData.choices[0]?.text || 'No answer found';
 
-        setCorrectAnswer(correctAnswerText);
+        // 正解をセット
+        setCorrectAnswers([correctAnswerText]);
 
-        // 回答選択肢を生成
+        // 回答選択肢を生成するAPIコールを行う
+        const answerOptionsPrompt = `Generate answer choices for this question: ${generatedQuestion}`;
         const answerOptionsResponse = await fetch('https://api.openai.com/v1/engines/davinci/completions', {
           method: 'POST',
           headers: {
@@ -64,19 +120,23 @@ const RightQuiz: React.FC = () => {
             'Authorization': `Bearer ${apiKey}`,
           },
           body: JSON.stringify({
-            prompt: `Generate answer choices for this question: ${data.choices[0].text}`,
+            prompt: answerOptionsPrompt,
             max_tokens: 30,
             temperature: 0.7,
           }),
         });
+
         const answerOptionsData = await answerOptionsResponse.json();
         const answerOptionsList = answerOptionsData.choices.map((choice: Choice) => choice.text);
 
-        setAnswerOptions(answerOptionsList);
+        // 選択肢をシャッフルしてセット
+        const shuffledOptions = shuffleArray(answerOptionsList);
+        setAnswerOptions(shuffledOptions);
 
-        setSelectedAnswer('');
+        setSelectedAnswers([]);
         setShowResult(false);
         setIsCorrect(false);
+        setIsRainingStars(false);
       } else {
         console.error('問題の生成に失敗しました:', data);
       }
@@ -88,82 +148,200 @@ const RightQuiz: React.FC = () => {
   };
 
   const handleAnswerChange = (event: ChangeEvent<HTMLInputElement>) => {
-    setSelectedAnswer(event.target.value);
+    const value = event.target.value;
+    const isChecked = selectedAnswers.includes(value);
+    if (isChecked) {
+      // 既に選択されていた場合、選択を解除
+      setSelectedAnswers(selectedAnswers.filter((answer) => answer !== value));
+    } else {
+      // 選択されていなかった場合、選択
+      setSelectedAnswers([...selectedAnswers, value]);
+    }
   };
 
   const handleCheckAnswer = () => {
     setShowResult(true);
-    if (selectedAnswer === correctAnswer) {
+    const isAnswerCorrect = arraysEqual(selectedAnswers.sort(), correctAnswers.sort());
+    if (isAnswerCorrect) {
       setIsCorrect(true);
+      setScore(score + 1);
+      setIsRainingStars(true);
     }
+    setQuestionCount(questionCount + 1);
   };
 
-  const handleGenerateQuestion = () => {
-    setInitialLoad(false);
-    generateQuestion();
+  const arraysEqual = (arr1: string[], arr2: string[]) => {
+    return arr1.length === arr2.length && arr1.every((value, index) => value === arr2[index]);
+  };
+
+  const shuffleArray = (array: string[]) => {
+    const shuffledArray = [...array];
+    for (let i = shuffledArray.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [shuffledArray[i], shuffledArray[j]] = [shuffledArray[j], shuffledArray[i]];
+    }
+    return shuffledArray;
   };
 
   useEffect(() => {
-    if (!initialLoad) {
-      // 初回のレンダリング時以外に問題を生成
-      generateQuestion();
+    if (!quizStarted && !shouldRestartQuiz) {
+      setOpenDialog(false);
     }
-  }, [initialLoad]);
+  }, [quizStarted, shouldRestartQuiz]);
+
+  useEffect(() => {
+    if (!quizStarted) {
+      setShouldRestartQuiz(false);
+    }
+  }, [quizStarted]);
 
   return (
     <Container maxWidth="sm">
       <Typography variant="h5" align="center" gutterBottom>
-        問題
+        Quiz
       </Typography>
-      <Typography variant="body1" gutterBottom>
-        以下の問題に回答してください：
-      </Typography>
-      <Typography variant="body1" gutterBottom>
-        {question}
-      </Typography>
-      <RadioGroup value={selectedAnswer} onChange={handleAnswerChange}>
-        {answerOptions.map((option, index) => (
-          <FormControlLabel
-            key={index}
-            value={option}
-            control={<Radio color="primary" />}
-            label={option}
-            labelPlacement="end"
-          />
-        ))}
-      </RadioGroup>
-      {!initialLoad && !showResult && (
-        <Button variant="contained" color="primary" onClick={handleCheckAnswer}>
-          回答を確認
-        </Button>
+
+      {!quizStarted && !shouldRestartQuiz && (
+        <Box
+          display="flex"
+          flexDirection="column"
+          alignItems="center"
+          justifyContent="center"
+        >
+          <Button
+            variant="contained"
+            color="primary"
+            onClick={handleStartQuiz}
+            style={{ marginTop: '20px', marginBottom: '20px' }}
+          >
+            クイズを始める
+          </Button>
+        </Box>
       )}
-      {!showResult && (
-        <Button variant="contained" color="primary" onClick={handleGenerateQuestion}>
-          問題を解く
-        </Button>
-      )}
-      {showResult && (
-        <div>
-          {isCorrect ? (
-            <Typography variant="body1" color="primary" gutterBottom>
-              正解です！
-            </Typography>
-          ) : (
-            <Typography variant="body1" color="error" gutterBottom>
-              不正解です。
-            </Typography>
-          )}
-          <LinearProgress
-            variant="determinate"
-            value={isCorrect ? 100 : 0}
-          />
-        </div>
-      )}
-      {loading && (
+
+      {quizStarted && loading && (
         <Typography variant="body1" align="center" gutterBottom>
           問題を生成中...
         </Typography>
       )}
+
+      {quizStarted && !loading && !shouldRestartQuiz && (
+        <Box
+          display="flex"
+          flexDirection="column"
+          alignItems="center"
+          justifyContent="center"
+        >
+          <Typography variant="body1" gutterBottom>
+            以下の問題に回答してください：
+          </Typography>
+          <Typography variant="body1" gutterBottom>
+            {question}
+          </Typography>
+          <Typography variant="body1" gutterBottom>
+            (正解は複数選択可)
+          </Typography>
+          <FormGroup>
+            <Grid container spacing={1}>
+              {answerOptions.map((option, index) => (
+                <Grid item xs={3} key={index}>
+                  <Checkbox
+                    value={option}
+                    color="primary"
+                    checked={selectedAnswers.includes(option)}
+                    onChange={handleAnswerChange}
+                  />
+                  {option}
+                </Grid>
+              ))}
+            </Grid>
+          </FormGroup>
+          {!showResult && (
+            <Button
+              variant="contained"
+              color="primary"
+              onClick={handleCheckAnswer}
+              startIcon={<CheckIcon />}
+              style={{ marginTop: '20px', marginBottom: '20px' }}
+            >
+              回答する
+            </Button>
+          )}
+          {showResult && (
+            <div>
+              {isCorrect ? (
+                <Typography variant="body1" color="primary" gutterBottom>
+                  正解です！
+                </Typography>
+              ) : (
+                <Typography variant="body1" color="error" gutterBottom>
+                  不正解です。正解は {correctAnswers.join(', ')} です。
+                </Typography>
+              )}
+              <LinearProgress
+                variant="determinate"
+                value={(score / 10) * 100}
+                style={{ marginTop: '20px', marginBottom: '20px' }}
+              />
+              {isRainingStars && (
+                <div className="stars-container">
+                  {/* 星アイコンを表示する */}
+                  <StarIcon />
+                  <StarIcon />
+                  <StarIcon />
+                  <StarIcon />
+                </div>
+              )}
+            </div>
+          )}
+          <Button
+            variant="contained"
+            color="primary"
+            onClick={handleNextQuestion}
+            disabled={!showResult}
+            endIcon={<ArrowForwardIcon />}
+            style={{ marginTop: '20px', marginBottom: '20px' }}
+          >
+            {questionCount === 9 ? '結果を表示' : '次の問題へ'}
+          </Button>
+        </Box>
+      )}
+
+      {quizStarted && shouldRestartQuiz && (
+        <Box
+          display="flex"
+          flexDirection="column"
+          alignItems="center"
+          justifyContent="center"
+        >
+          <Button
+            variant="contained"
+            color="primary"
+            onClick={handleRestartQuiz}
+            style={{ marginTop: '20px', marginBottom: '20px' }}
+          >
+            新しいクイズを解く
+          </Button>
+        </Box>
+      )}
+
+      <Dialog
+        open={openDialog}
+        onClose={handleCloseDialog}
+        aria-labelledby="quiz-dialog-title"
+      >
+        <DialogTitle id="quiz-dialog-title">クイズ結果</DialogTitle>
+        <DialogContent>
+          <DialogContentText>
+            {`正解数: ${score} / 10`}
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleCloseDialog} color="primary">
+            閉じる
+          </Button>
+        </DialogActions>
+      </Dialog>
     </Container>
   );
 };
