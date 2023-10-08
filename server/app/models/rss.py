2355a094f61cc2f5dd926913e7c867d0feafca67
Reiji Kume <83393479+reiji061@users.noreply.github.com>
2023-10-09 05:47:21 +0900

    rss_feedのimage_urlの追加

--- server/app/models/rss.py
+++ server/app/models/rss.py
@@ -1,11 +1,11 @@
 from pydantic import BaseModel
 from app.internal.util import strip_html_tags
-
+from typing import List, Optional
 
 class RssFetchRequest(BaseModel):
-    urls: list[str]
+    urls: List[str]
     limit: int = 10
-
+    image_url: Optional[str] = None
 
 class RssFeedModel(BaseModel):
     title: str
@@ -13,15 +13,29 @@ class RssFeedModel(BaseModel):
     description: str
     published: str
     tags: list[str]
+    image_url: str | None
 
     @staticmethod
     def from_entry(entry):
+        image_url = None
+        if 'enclosures' in entry:
+            for enclosure in entry.enclosures:
+                if enclosure.type.startswith('image/'):
+                    image_url = enclosure.href
+                    break
+        elif 'media_content' in entry:
+            for media in entry.media_content:
+                if media['type'].startswith('image/'):
+                    image_url = media['url']
+                    break
+        
         return RssFeedModel(
             title=strip_html_tags(entry.title),
             link=entry.link,
             description=strip_html_tags(entry.description),
             published=entry.published,
             tags=[tag.term for tag in entry.tags],
+            image_url=image_url
         )
 
 
