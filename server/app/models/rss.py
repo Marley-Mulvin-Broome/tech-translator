6c9d34d8d70f84b9530f87eedb7a66decdba8df4
Marley Mulvin Broome <z5444540@ad.unsw.edu.au>
2023-10-09 12:18:02 +0900

    RSS XDDDFSDFJES

--- server/app/models/rss.py
+++ server/app/models/rss.py
@@ -2,10 +2,22 @@ from pydantic import BaseModel
 from app.internal.util import strip_html_tags
 from typing import List, Optional
 
+
 class RssFetchRequest(BaseModel):
     urls: List[str]
     limit: int = 10
     image_url: Optional[str] = None
+
+
+def extract_image_url(entry):
+    img_url = ""
+
+    if "media_content" in entry:
+        for media in entry.media_content:
+            img_url = media["url"]
+
+    return img_url
+
 
 class RssFeedModel(BaseModel):
     title: str
@@ -17,25 +29,15 @@ class RssFeedModel(BaseModel):
 
     @staticmethod
     def from_entry(entry):
-        image_url = None
-        if 'enclosures' in entry:
-            for enclosure in entry.enclosures:
-                if enclosure.type.startswith('image/'):
-                    image_url = enclosure.href
-                    break
-        elif 'media_content' in entry:
-            for media in entry.media_content:
-                if media['type'].startswith('image/'):
-                    image_url = media['url']
-                    break
-        
+        image_url = extract_image_url(entry)
+
         return RssFeedModel(
             title=strip_html_tags(entry.title),
             link=entry.link,
             description=strip_html_tags(entry.description),
             published=entry.published,
             tags=[tag.term for tag in entry.tags],
-            image_url=image_url
+            image_url=image_url,
         )
 
 
